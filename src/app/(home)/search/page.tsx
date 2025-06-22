38a96d88571447fa6dbceced4b4b5d3b1ac61056
Zhdanovich <Zhiv80@mail.ru>
2025-06-22 18:13:48 +0300

    Feat: add: reset styles, pages styles, address card component & interface, api const & query

--- src/app/(home)/search/page.tsx
+++ src/app/(home)/search/page.tsx
@@ -1,5 +1,11 @@
 import styles from "./searchPage.module.scss";
 
 export default function Search() {
-	return <h1>Поиск по id</h1>;
+	return (
+		<div className={styles.searchPageWrap}>
+			<div className={styles.searchPageContainer}>
+				<h1 className={styles.searchPageTitle}>Поиск адреса по id</h1>
+			</div>
+		</div>
+	);
 }
---
+++ src/services/api/apiConstants.ts
@@ -0,0 +1,1 @@
+export const addressesEndPoint = "api/Addresses";
